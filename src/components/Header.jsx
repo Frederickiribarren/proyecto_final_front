cd317163de1176cafdb0a86e22a488a1b1b7bb5a
Frederick Iribarren <104109101+Frederickiribarren@users.noreply.github.com>
2025-06-19 18:16:51 -0400

    se modifica logo barra navegacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,7 +71,7 @@ function Header(props) {
         elevation={2}
         sx={{
           
-          backgroundColor: "#ff3f",
+          backgroundColor: "#ffff",
           boxShadow: "0 2px 8px rgba(0,0,0,0.04)",
           borderBottom: "1px solid #e0e0e0",
         }}
